a61689a02913b19d8ea8efd391a15b2f2bc9c1d6
momezo <sambemomar@icloud.com>
2021-04-16 15:34:12 +0200

    DEbut

--- Tapatan_sujet.py
+++ Tapatan_sujet.py
@@ -1,3 +1,8 @@
-debut
-test 1
-test 2
+from tkinter import *
+from tkinter.messagebox import *
+def Clic(event):
+    global a,C1,C2,C3,C1RC,C1R,C2RC,C2R,C3RC,C3R,L1RC,L1R,L2RC,L2R,L3RC,L3R,L1,L2,L3
+X = event.x
+Y = event.y
+
+mainloop()
